47965afc974a7c8fed735ea76311cec5d220a0ad
Brendan P <64713032+brendisurfs@users.noreply.github.com>
2023-01-31 10:49:09 -0800

    think I fixed my formatter issue
    Signed-off-by: Brendan P <64713032+brendisurfs@users.noreply.github.com>

--- after/plugin/formatter.lua
+++ after/plugin/formatter.lua
@@ -1,35 +1,35 @@
-local util = require("formatter.util")
 require("formatter").setup({
-  filetype = {
-    typescript = {
-      require("formatter.filetypes.typescript").prettierd,
-    },
-    typescriptreact = {
-      require("formatter.filetypes.typescriptreact").prettierd,
-    },
-    lua = {
-      require("formatter.filetypes.lua").stylua,
-    },
-    ["*"] = {
-      require("formatter.filetypes.any").remove_trailing_whitespace,
-    },
-  },
+	filetype = {
+		typescript = {
+			require("formatter.filetypes.typescript").prettierd,
+		},
+		typescriptreact = {
+			require("formatter.filetypes.typescriptreact").prettierd,
+		},
+		lua = {
+			require("formatter.filetypes.lua").stylua,
+		},
+		["*"] = {
+			require("formatter.filetypes.any").remove_trailing_whitespace,
+		},
+	},
 })
 
 local augroup = vim.api.nvim_create_augroup
-augroup("fmt", {})
 local autocmd = vim.api.nvim_create_autocmd
+
+augroup("fmt", {})
 --
 autocmd({ "BufWritePre" }, {
-  group = "fmt",
-  pattern = "*",
-  callback = function()
-    vim.lsp.buf.format({ async = false })
-  end
+	group = "fmt",
+	pattern = "*",
+	callback = function()
+		vim.cmd([[Format]])
+	end,
 })
 -- Highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
--- init.lua
+++ init.lua
@@ -1,25 +1,3 @@
 require('brendi.packer')
 require('brendi.remap')
 require('brendi.set')
-
--- local set = vim.opt
--- set.nu = true
--- set.relativenumber = true
---
---
--- vim.g.tabstop = 2
--- set.exrc = true
--- set.wrap = false
--- set.backup = false
--- set.undofile = true
--- set.incsearch = true
--- set.undodir = vim.fn.expand('~/.vim/undodir')
---
--- set.tabstop = 2
--- set.softtabstop = 2
--- set.scrolloff = 8
--- set.shiftwidth = 2
--- vim.cmd('set expandtab')
--- set.signcolumn = "yes"
--- set.termguicolors = true
---
