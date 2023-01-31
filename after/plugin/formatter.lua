fdb90c463dc9c06e70d81bed1f4bd05f4d10158b
Brendan P <64713032+brendisurfs@users.noreply.github.com>
2023-01-31 10:50:04 -0800

    revert to lsp formatting
    Signed-off-by: Brendan P <64713032+brendisurfs@users.noreply.github.com>

--- after/plugin/formatter.lua
+++ after/plugin/formatter.lua
@@ -1,18 +1,18 @@
 require("formatter").setup({
-	filetype = {
-		typescript = {
-			require("formatter.filetypes.typescript").prettierd,
-		},
-		typescriptreact = {
-			require("formatter.filetypes.typescriptreact").prettierd,
-		},
-		lua = {
-			require("formatter.filetypes.lua").stylua,
-		},
-		["*"] = {
-			require("formatter.filetypes.any").remove_trailing_whitespace,
-		},
-	},
+  filetype = {
+    typescript = {
+      require("formatter.filetypes.typescript").prettierd,
+    },
+    typescriptreact = {
+      require("formatter.filetypes.typescriptreact").prettierd,
+    },
+    lua = {
+      require("formatter.filetypes.lua").stylua,
+    },
+    ["*"] = {
+      require("formatter.filetypes.any").remove_trailing_whitespace,
+    },
+  },
 })
 
 local augroup = vim.api.nvim_create_augroup
@@ -21,15 +21,15 @@ local autocmd = vim.api.nvim_create_autocmd
 augroup("fmt", {})
 --
 autocmd({ "BufWritePre" }, {
-	group = "fmt",
-	pattern = "*",
-	callback = function()
-		vim.cmd([[Format]])
-	end,
+  group = "fmt",
+  pattern = "*",
+  callback = function()
+    vim.lsp.buf.format()
+  end
 })
 -- Highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+  callback = function()
+    vim.highlight.on_yank()
+  end,
 })
