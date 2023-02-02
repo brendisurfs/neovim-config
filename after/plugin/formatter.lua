55a4d23622c20825f77114716d3f91d94c13fa2f
Brendan P <64713032+brendisurfs@users.noreply.github.com>
2023-02-01 18:59:43 -0800

    more formatter setup
    Signed-off-by: Brendan P <64713032+brendisurfs@users.noreply.github.com>

--- after/plugin/formatter.lua
+++ after/plugin/formatter.lua
@@ -1,49 +1,31 @@
-local format = function(payload)
-  vim.lsp.buf.format({
-    filter = function(client)
-      print(client[1])
-      return client.name ~= "volar"
-    end,
-  })
-end
-
-require("formatter").setup({
-  filetype = {
+require('lsp-format').setup {
+    sync = false,
     lua = {
-      require("formatter.filetypes.lua").stylua,
+        require("formatter.filetypes.lua").stylua,
+    },
+    css = {
+        require('formatter.filetypes.css').prettier,
     },
     typescript = {
-      require("formatter.filetypes.typescript").prettier,
+        require("formatter.filetypes.typescript").prettier,
     },
     typescriptreact = {
-      require("formatter.filetypes.typescriptreact").prettier,
+        require("formatter.filetypes.typescriptreact").prettier,
     },
     ["*"] = {
-      require("formatter.filetypes.any").remove_trailing_whitespace,
+        require("formatter.filetypes.any").remove_trailing_whitespace,
     },
-  },
-})
-
-local augroup = vim.api.nvim_create_augroup
-local autocmd = vim.api.nvim_create_autocmd
-
-augroup("fmt", {})
---
-autocmd({ "BufWritePre" }, {
-  group = "fmt",
-  pattern = "*",
-  command = "Format"
-})
+}
 
 -- Highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+    callback = function()
+        vim.highlight.on_yank()
+    end,
 })
 
 require("indent_blankline").setup({
-  space_char_blankline = " ",
-  show_current_context = true,
-  show_current_context_start = true,
+    space_char_blankline = " ",
+    show_current_context = true,
+    show_current_context_start = true,
 })
--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,74 +1,78 @@
+require('mason').setup()
 require("gitsigns").setup()
-local lsp = require("lsp-zero")
-lsp.preset("recommended")
+require("mason-lspconfig").setup()
+local lsp = require('mason-lspconfig')
+local lspconfig = require('lspconfig')
 
-lsp.configure("rust_analyzer", {
-	capabilities = {
-		textDocument = {
-			completion = {
-				completionItem = {
-					snippetSupport = false,
-				},
-			},
-		},
-	},
-})
+local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
+local servers = lsp.get_installed_servers()
+
+
+for _, server_name in pairs(servers) do
+    if server_name == "rust_analyzer" then
+        lspconfig["rust_analyzer"].setup {
+            capabilities = {
+                textDocument = {
+                    completion = {
+                        completionItem = {
+                            snippetSupport = false,
+                        },
+                    },
+                },
+            },
+        }
+    end
+    lspconfig[server_name].setup({
+        on_attach = function(client)
+            require('lsp-format').on_attach(client)
+        end,
+        capabilities = lsp_capabilities
+    })
+end
 
 local cmp = require("cmp")
 local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 local cmp_select = { behavior = cmp.SelectBehavior.Insert }
-local cmp_mappings = lsp.defaults.cmp_mappings({
-	["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-	["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-	["<C-f>"] = cmp.mapping.confirm({ select = true }),
-	["<C-space>"] = cmp.mapping.complete(),
-})
 vim.opt.updatetime = 100
 
 cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 
-lsp.set_preferences({
-	sign_icons = {},
-})
 
-lsp.setup_nvim_cmp({
-	mapping = cmp_mappings,
-})
-
-local entry_filter = function(entry, ctx)
-	local kind = cmp.types.lsp.CompletionItemKind[entry:get_kind()]
-	print(kind)
-	if kind == "Text" then
-		return false
-	end
-	return true
-end
 
 cmp.setup({
-	sources = {
-		{
-			name = "nvim_lsp",
-			priority = 8,
-			entry_filter = entry_filter,
-		},
-		{
-			name = "nvim_lua",
-			priority = 5,
-			entry_filter = entry_filter,
-		},
-		{
-			name = "buffer",
-			priority = 7,
-		},
-		{
-			name = "path",
-			entry_filter = entry_filter,
-		},
-		{
-			name = "luasnip",
-			priority = 5,
-		},
-	},
+    snippet = {
+        expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+        end
+    },
+    sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "nvim_lua" },
+        { name = "buffer" },
+        { name = "path" },
+        { name = 'luasnip' }
+    }),
+    mapping = cmp.mapping.preset.insert({
+        ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+        ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+        ["<C-f>"] = cmp.mapping.confirm({ select = true }),
+        ["<C-space>"] = cmp.mapping.complete(),
+    }),
 })
 
-lsp.setup()
+
+--- EFM for formatting
+--
+local prettier = {
+    formatCommand = [[Prettier]]
+}
+lspconfig.efm.setup {
+    on_attach = require("lsp-format").on_attach,
+    init_options = { documentFormatting = true },
+    settings = {
+        languages = {
+            typescript = { prettier }
+        }
+    }
+
+}
