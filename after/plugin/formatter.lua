b5ec2b238d3ca4d6230a20ab514cf4be8a12fe0f
Brendan P <64713032+brendisurfs@users.noreply.github.com>
2023-02-15 08:16:15 -0800

    feat: changed to coc.nvim for faster completion
    Signed-off-by: Brendan P <64713032+brendisurfs@users.noreply.github.com>

--- after/plugin/formatter.lua
+++ after/plugin/formatter.lua
@@ -6,8 +6,11 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 })
 
 vim.opt.list = true
+-- sets blank line to only context highlight.
+vim.cmd [[highlight IndentBlanklineChar guifg=#191724 gui=nocombine]]
 require("indent_blankline").setup({
     space_char_blankline = " ",
     show_current_context = true,
     show_current_context_start = true,
 })
+
--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,104 +1,100 @@
-require("mason").setup()
-require("gitsigns").setup()
-require('lsp-format').setup()
-require("mason-lspconfig").setup()
-local lsp = require("mason-lspconfig")
-local lspconfig = require("lspconfig")
-
-local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
-local servers = lsp.get_installed_servers()
-
-for _, server_name in pairs(servers) do
-    if server_name == "rust_analyzer" then
-        lspconfig["rust_analyzer"].setup({
-            capabilities = {
-                textDocument = {
-                    completion = {
-                        completionItem = {
-                            snippetSupport = false,
-                        },
-                    },
-                },
-            },
-        })
-    end
-    lspconfig[server_name].setup({
-        on_attach = require('lsp-format').on_attach,
-        capabilities = lsp_capabilities,
-    })
-end
+-- require("mason").setup()
+-- require("gitsigns").setup()
+-- require('lsp-format').setup()
+-- require("mason-lspconfig").setup()
+-- local lsp = require("mason-lspconfig")
+-- local lspconfig = require("lspconfig")
+--
+-- local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
+-- local servers = lsp.get_installed_servers()
+--
+-- for _, server_name in pairs(servers) do
+--     if server_name == "rust_analyzer" then
+--         lspconfig["rust_analyzer"].setup({
+--             capabilities = {
+--                 textDocument = {
+--                     completion = {
+--                         completionItem = {
+--                             snippetSupport = false,
+--                         },
+--                     },
+--                 },
+--             },
+--         })
+--     end
+--     lspconfig[server_name].setup({
+--         on_attach = require('lsp-format').on_attach,
+--         capabilities = lsp_capabilities,
+--     })
+-- end
 
 vim.diagnostic.config({
     virtual_text = false,
 })
 
 
-local cmp = require("cmp")
-local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-local handlers = require("nvim-autopairs.completion.handlers")
-
-local cmp_select = { behavior = cmp.SelectBehavior.Insert }
-vim.opt.updatetime = 100
-
-cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({
-    filetypes = {
-        ["*"] = {
-            ["<"] = {
-                kind = {
-                    cmp.lsp.CompletionItemKind.Method,
-                    cmp.lsp.CompletionItemKind.Function,
-                },
-                handler = handlers["*"],
-            }
-        }
-    }
-}))
-
-cmp.setup({
-    snippet = {
-        expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-        end,
-    },
-    sources = cmp.config.sources({
-        { name = "nvim_lsp" },
-        { name = "nvim_lua" },
-        { name = "buffer" },
-        { name = "path" },
-        { name = "luasnip" },
-    }),
-    mapping = cmp.mapping.preset.insert({
-        ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-        ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-        ["<C-f>"] = cmp.mapping.confirm({ select = true }),
-        ["<C-space>"] = cmp.mapping.complete(),
-    }),
-})
+-- local cmp = require("cmp")
+-- local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+-- local handlers = require("nvim-autopairs.completion.handlers")
+--
+-- local cmp_select = { behavior = cmp.SelectBehavior.Insert }
+-- vim.opt.updatetime = 100
+--
+-- cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({
+--     filetypes = {
+--         ["*"] = {
+--             ["<"] = {
+--                 kind = {
+--                     cmp.lsp.CompletionItemKind.Method,
+--                     cmp.lsp.CompletionItemKind.Function,
+--                 },
+--                 handler = handlers["*"],
+--             }
+--         }
+--     }
+-- }))
+--
+-- cmp.setup({
+--     snippet = {
+--         expand = function(args)
+--             require("luasnip").lsp_expand(args.body)
+--         end,
+--     },
+--     sources = cmp.config.sources({
+--         { name = "nvim_lsp" },
+--         { name = "nvim_lua" },
+--         { name = "buffer" },
+--         { name = "path" },
+--         { name = "luasnip" },
+--     }),
+--     mapping = cmp.mapping.preset.insert({
+--         ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+--         ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+--         ["<C-f>"] = cmp.mapping.confirm({ select = true }),
+--         ["<C-space>"] = cmp.mapping.complete(),
+--     }),
+-- })
 
 --- EFM for formatting
 
-require("lsp-format").setup {
-    sync = false,
-}
+-- require("lsp-format").setup {
+--     sync = false,
+-- }
+--
+-- local prettier = {
+--     formatCommand = [[prettier --stdin-filepath ${INPUT}]],
+--     formatStdin = true
+-- }
 
-local prettier = {
-    formatCommand = [[prettier --stdin-filepath ${INPUT}]],
-    formatStdin = true
-}
-
-local taplo = {
-    formatCommand = [[taplo fmt --]]
-}
-
-require("lspconfig").efm.setup {
-    on_attach = require("lsp-format").on_attach,
-    init_options = { documentFormatting = true },
-    settings = {
-        languages = {
-            typescriptreact = { prettier },
-            typescript = { prettier },
-            json = { prettier },
-            css = { prettier },
-        }
-    }
-}
+-- require("lspconfig").efm.setup {
+--     on_attach = require("lsp-format").on_attach,
+--     init_options = { documentFormatting = true },
+--     settings = {
+--         languages = {
+--             typescriptreact = { prettier },
+--             typescript = { prettier },
+--             json = { prettier },
+--             css = { prettier },
+--         }
+--     }
+-- }
